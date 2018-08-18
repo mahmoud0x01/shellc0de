73e07cbb8e7cc49599c8ce49d58a73ae9f4b609a
mahmoud adel <30966854+mahmoud0x01@users.noreply.github.com>
2018-08-18 21:41:18 +0200

    Update shellc0de.c

--- shellc0de.c
+++ shellc0de.c
@@ -7,10 +7,10 @@ char code[] =
 "\x59\x31" 
 "\xd2\xb2"
 "\x16\xcd\x80" // call kernel int 80h
-"\xe8\xec\xff\xff\xff\x79\x30"     /* the message to write */
+"\xe8\xec\xff\xff\xff\x79\x30"     /* the message binary calculation to pass to ecx register */
 "\x75\x20\x73\x70\x34\x33\x6b\x20"
 "\x4f\x50\x43\x30\x44\x45\x20\x21"
-"\x3f\xe2\xe6"; //infinite loop
+"\x3f\xe2\xe6"; //infinite loop / call 0x11
 
 int main(int argc, char **argv)
 {
